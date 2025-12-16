2df889a27ebb0205551272281d84227a6a4281fb
younggeun <qwazsx1346@gmail.com>
2025-12-16 17:22:35 +0900

    더하기빼기도 못한날

---
+++ fe/js/ch18/condition_exp.js
@@ -0,0 +1,8 @@
+/* condition_exp.js
+조건식(삼항식), 분기식
+식 ? true일 때 : false일 때
+
+*/
+let val = 10;
+const result = val % 2 == 0 ? '홀수' : '짝수';
+console.log('result:', result);
---
+++ fe/js/ch18/mission04.js
@@ -0,0 +1,6 @@
+
+
+let val = -20;
+
+const result = val >= 0 ? val : -val;
+console.log('result:', result);
