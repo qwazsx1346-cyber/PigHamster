438e69a269f96267bc44db727f0c81a76e1b6891
younggeun <qwazsx1346@gmail.com>
2026-01-02 12:03:49 +0900

    forEach구문 재학습(중요)

--- fe/js/storage/storage_method03.js
+++ fe/js/storage/storage_method03.js
@@ -1,13 +1,17 @@
 const arr = [
-    { id: 'abc', name: '홍길동1' },
-    { id: 'bcd', name: '홍길동2' },
-    { id: 'cde', name: '홍길동3' }
+  { id: 'abc', name: '홍길동1' },
+  { id: 'bcd', name: '홍길동2' },
+  { id: 'cde', name: '홍길동3' },
 ];
 const obj = {};
-for (let i=0; i<arr.length; i++) {
-  const key = arr[i].id;
-  obj[key] = arr[i];
-};
+// for (let i=0; i<arr.length; i++) {
+//   obj[arr[i].id] = arr[i];
+// };
+// console.log(obj);
+
+arr.forEach( item => {
+  obj[item.id] = item
+});
 console.log(obj);
 
 // const obj = {
@@ -15,3 +19,5 @@ console.log(obj);
 //     bcd: { id: 'bcd', name: '홍길동2' },
 //     cde: { id: 'cde', name: '홍길동3' }
 // };
+
+//forEach함수에서 forEach(a,b)에서 b는 방번호, a는 객체가 나오는건 약속된 함수임.
